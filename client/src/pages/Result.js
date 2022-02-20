91068247232dbd625dac073605d7a7661c401f28
Peter Ke <peterke0911@gmail.com>
2022-02-20 09:59:54 -0500

    Add progress eta, link

--- client/src/pages/Result.js
+++ client/src/pages/Result.js
@@ -1,20 +1,22 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 // import Box from "@mui/material/Box";
 // import Grid from "@mui/material/Grid";
-// import Typography from "@mui/material/Typography";
+import Typography from "@mui/material/Typography";
 import Puffer from "../components/Puffer";
 
-import "./style.css"
+import "./style.css";
 
-import logo from "../img/puffer_logo.png"
+import logo from "../img/puffer_logo.png";
 
-import { getStatus } from "../helpers/endpoints";
+import { getStatus, getAudioLink } from "../helpers/endpoints";
 import ResultComponent from "../components/Result";
 
 const Result = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [status, setStatus] = useState(null);
+  const [audioDuration, setAudioDuration] = useState(null);
   const isComplete =
     status && (status.status === "completed" || status.status === "error");
 
@@ -34,16 +36,33 @@ const Result = () => {
     return () => clearInterval(interval);
   }, [id, isComplete]);
 
+  if (status !== null && !isComplete) {
+    const audio = new Audio(getAudioLink(status.id));
+    audio.onloadedmetadata = () => {
+      setAudioDuration(audio.duration);
+    };
+  }
+
   if (status === null || !isComplete) {
-    document.body.style = 'background: #020887;';
+    document.body.style = "background: #020887;";
     return (
       <div>
         <Puffer></Puffer>
-        <p class="loading"><b>Loading...</b></p>
+        <p class="loading">
+          <b>Loading...</b>
+        </p>
+        {audioDuration === null ? (
+          ""
+        ) : (
+          <Typography color="white">
+            This will take approximately{" "}
+            {Math.floor((audioDuration * 0.3) / 60) || 1} minutes to process.
+          </Typography>
+        )}
       </div>
     );
   } else if (status.status === "error") {
-    document.body.style = 'background: white;';
+    document.body.style = "background: white;";
     return (
       <div>
         <h1>Error</h1>
@@ -51,15 +70,20 @@ const Result = () => {
       </div>
     );
   } else {
-    document.body.style = 'background: white;';
+    document.body.style = "background: white;";
     return (
       <div className="page">
         <div className="page-head">
-           <img className="image-container" src={logo} alt="logo"></img>
-           </div>
+          <img
+            onClick={() => navigate("/")}
+            style={{ cursor: "pointer" }}
+            className="image-container"
+            src={logo}
+            alt="logo"
+          ></img>
+        </div>
         <ResultComponent status={status} />
       </div>
-
     );
   }
 };
