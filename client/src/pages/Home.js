80830a5e8d89faab1f7ebf4bc36eb0cc030bfead
Carmen Law <carmenlaw08@gmail.com>
2022-02-19 22:45:28 -0800

    afadfsfsd

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import NavButton from '../components/NavButton';
 import UploadBox from '../components/UploadBox';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import logo from '../img/puffer_logo.png';
+import clip from '../img/clip-01.png';
 import CollectionsBookmarkIcon from '@mui/icons-material/CollectionsBookmark';
 import BorderColorSharpIcon from '@mui/icons-material/BorderColorSharp';
 import RecordVoiceOverSharpIcon from '@mui/icons-material/RecordVoiceOverSharp';
@@ -41,6 +42,7 @@ const Home = () => {
               <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
           </svg>
         </div>
+        <img src={clip} alt="decoPic" align="right"></img>
       </section>
 
       <section className="white-background">
